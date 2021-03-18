a0bbce85a6d903cfac769f00880056aa6922d24c
Elizabeth Kovtun <58321670+elizabethkovtun@users.noreply.github.com>
2021-03-18 14:53:12 +0200

    Add files via upload

---
+++ Half2/Лр2/Lab_2_3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Lab_2_3
+{
+    public delegate void Division(int[,] array);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = { { 1, 10, 9, 14 }, { 19, 21, 90, 40 }, { 49, 70, 47, 33 }, { 89, 36, 33, 88 } };
+            Console.WriteLine("Масив чисел: ");
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(array[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Division div3 = new Division(Division_on_three);
+            Division div7 = new Division(Division_on_seven);
+            Division division = div3 + div7;
+            division(array);
+
+
+        }
+        public static void Division_on_three(int[,] array)
+        {
+            Console.WriteLine("Числа, що діляться на 3: ");
+            foreach (int c in array)
+            {
+                if (c % 3 == 0)
+                {
+                    Console.Write(c + "  ");
+                }
+            }
+            Console.WriteLine();
+        }
+        public static void Division_on_seven(int[,] array)
+        {
+            Console.WriteLine("Числа, що діляться на 7: ");
+            foreach (int c in array)
+            {
+                if (c % 7 == 0)
+                {
+                    Console.Write(c + "  ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
