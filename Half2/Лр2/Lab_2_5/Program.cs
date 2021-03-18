5ff72f9e18dd91eb4f2720415255304fdf53def8
Elizabeth Kovtun <58321670+elizabethkovtun@users.noreply.github.com>
2021-03-18 14:54:38 +0200

    Add files via upload

---
+++ Half2/Лр2/Lab_2_5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+
+namespace Lab_2_5
+{
+    class MainClass
+    {
+        public  void Main(string[] args)
+        {
+            
+
+            while(true)
+            {
+                try
+                {
+                    string st = Console.ReadLine();
+                    int a = st[0];
+                    int b = st[2];
+                    string met = "Method" + st[0] ;
+                    MethodInfo mi = this.GetType().GetMethod(met);
+                    mi.Invoke(this, b);
+                  
+                }
+                catch
+                {
+                    Console.WriteLine("Неправильний формат введення");
+                    break;
+                }
+
+              
+            }
+
+        }
+
+        public static double Method0(int n) => Math.Sqrt(n);
+        public static double Method1(int n) => n * n * n;
+        public static double Method2(int n)=> n+3.5;
+
+    }
+}
