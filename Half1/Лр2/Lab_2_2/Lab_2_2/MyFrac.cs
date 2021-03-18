5b7dd385493732ae2a1bd110231120160facc3dc
Elizabeth Kovtun <58321670+elizabethkovtun@users.noreply.github.com>
2021-03-18 15:49:26 +0200

    Add files via upload

---
+++ Half1/Лр2/Lab_2_2/Lab_2_2/MyFrac.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace Lab_2_2
+{
+    public class MyFrac
+    {
+        private int nom;
+        private int denom;
+
+        public MyFrac()
+        {
+            Random rand = new Random();
+            nom = rand.Next(1, 50);
+            denom = rand.Next(1, 50);
+            Console.WriteLine("Рандомно знаходимо дріб: {0}/{1}", nom, denom);
+        }
+        public MyFrac(int nom, int denom)
+        {
+            int CommonDenom = 1;
+            if (denom < 0)
+            {
+                denom = -denom;
+            }
+            if (nom < 0)
+            {
+                nom = -nom;
+            }
+            for (int i = 2; i <= nom; i++)
+            {
+                if (denom % i == 0 && nom % i == 0)
+                    CommonDenom = i;
+            }
+
+            nom = nom / CommonDenom;
+            denom = denom / CommonDenom;
+            Console.WriteLine(nom + "/" + denom);
+        }
+
+        public int Nom
+        {
+            get { return nom; }
+        }
+        public int Denom
+        {
+            get { return denom; }
+        }
+        public static void ToStringWithIntegerPart(int nom, int denom)
+        {
+            Console.WriteLine($"Дріб з виділеною цілою частиною: ({(nom) / (denom)}+{nom - (nom) / denom * denom}/{denom})");
+        }
+        public static void Plus(int nom1, int denom1, int nom2, int denom2)
+        {
+            Console.WriteLine("Результат додавання  двох дробів: " + (nom1 * denom2 + denom1 * nom2) + "/" + (denom1 * denom2));
+        }
+        public static void Minus(int nom1, int denom1, int nom2, int denom2)
+        {
+            Console.WriteLine("Результат віднімання двох дробів: " + (nom1 * denom2 - denom1 * nom2) + "/" + (denom1 * denom2));
+        }
+        public static void Multiply(int nom1, int denom1, int nom2, int denom2)
+        {
+            Console.WriteLine("Результат множення двох дробів: " + (nom1 * nom2) + "/" + (denom1 * denom2));
+        }
+        public static void Divide(int nom1, int denom1, int nom2, int denom2)
+        {
+            Console.WriteLine("Результат ділення двох дробів: " + (nom1 * denom2) + "/" + (denom1 * nom2));
+        }
+    }
+
+}
---
+++ Half1/Лр2/Lab_2_2/Lab_2_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lab_2_2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Перший  дріб");
+            MyFrac f1 = new MyFrac();
+            Console.Write("Cкорочений дріб: ");
+            MyFrac f11 = new MyFrac(f1.Nom, f1.Denom);
+            MyFrac.ToStringWithIntegerPart(f1.Nom, f1.Denom);
+            Console.WriteLine();
+            Console.WriteLine("Другий дріб");
+            MyFrac f2 = new MyFrac();
+            Console.Write("Cкорочений дріб: ");
+            MyFrac f22 = new MyFrac(f2.Nom, f2.Denom);
+            MyFrac.ToStringWithIntegerPart(f2.Nom, f2.Denom);
+            Console.WriteLine();
+
+            Console.WriteLine("Результат додавання  двох дробів: ");
+            MyFrac f3 = new MyFrac((f1.Nom * f2.Denom + f1.Denom * f2.Nom), (f1.Denom * f2.Denom));
+            Console.WriteLine("Результат віднімання  двох дробів: ");
+            MyFrac f4 = new MyFrac((f1.Nom * f2.Denom - f1.Denom * f2.Nom), (f1.Denom * f2.Denom));
+            Console.WriteLine("Результат множення  двох дробів: ");
+            MyFrac f5 = new MyFrac((f1.Nom * f2.Nom), (f1.Denom * f2.Denom));
+            Console.WriteLine("Результат ділення  двох дробів: ");
+            MyFrac f6 = new MyFrac((f1.Nom * f2.Denom), (f2.Nom * f1.Denom));
+        }
+    }
+}
