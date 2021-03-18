6338e0ef10063b76c0918050cdb200708ce6b25b
Elizabeth Kovtun <58321670+elizabethkovtun@users.noreply.github.com>
2021-03-18 14:52:41 +0200

    Add files via upload

---
+++ Half2/Лр2/Lab_2_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab_2_2
+{
+    delegate void Methods();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть кількість секунд: ");
+            int seconds = int.Parse(Console.ReadLine());
+            Method timer = new Method(seconds);
+            timer.Start();
+        }
+    }
+}
---
+++ Half2/Лр2/Lab_2_2/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab_2_2
+{
+    class Method
+    {
+        private int seconds { get; set; }
+        public Method(int _seconds)
+        {
+            this.seconds = _seconds;
+        }
+        public void Start()
+        {
+            Methods[] time = new Methods[] { Method1, Method2, Method3, Method4 };
+            Random rand = new Random();
+            while (true)
+            {
+                Thread.Sleep(seconds * 1000);
+                int a = rand.Next(0, 4);
+                time[a]();
+            }
+        }
+        private static void Method1() => Console.WriteLine("Був виконаний метод номер 1");
+        private static void Method2() => Console.WriteLine("Був виконаний метод номер 2");
+        private static void Method3() => Console.WriteLine("Був виконаний метод номер 3");
+        private static void Method4() => Console.WriteLine("Був виконаний метод номер 4");
+    }
+}
