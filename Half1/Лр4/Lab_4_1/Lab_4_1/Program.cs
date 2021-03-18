c90ecedaa4893b195fc286b51a3959076fe83711
Elizabeth Kovtun <58321670+elizabethkovtun@users.noreply.github.com>
2021-03-18 15:55:08 +0200

    Add files via upload

---
+++ Half1/Лр4/Lab_4_1/Lab_4_1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Lab_4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1;
+            string s2;
+            int a;//перше число
+            int b;//друге число
+            int m;//добуток 
+            long sum = 0;
+            int n = 0;//кількість нормальних файлів
+            for (int i = 10; i < 30; i++)
+            {
+                string path = @"Users\mac\Projects\Lab_4_1\Lab_4_1\txt" + i + ".txt";
+                try
+                {
+                    StreamReader f = new StreamReader(path);
+                    s1 = f.ReadLine();
+                    a = Convert.ToInt32(s1);
+                    s2 = f.ReadLine();
+                    b = Convert.ToInt32(s2);
+                    checked
+                    {
+                        m = a * b;
+                    }
+                    Console.WriteLine("Добуток чисел у файлі " + i + ".txt" + " становить: " + m);
+                    sum += m;
+                    n++;
+                    f.Close();
+                }
+                catch (System.IO.FileNotFoundException)
+                {
+                    using (StreamWriter no_file = new StreamWriter(@"Users\mac\Projects\Lab_4_1\Lab_4_1\txt\no_file.txt", true, System.Text.Encoding.Default))
+                    {
+                        no_file.WriteLine(i + ".rtf");
+                    }
+                }
+                catch (System.FormatException)
+                {
+                    using (StreamWriter bad_data = new StreamWriter(@"Users\mac\Projects\Lab_4_1\Lab_4_1\txt\bad_data.txt", true, System.Text.Encoding.Default))
+                    {
+                        bad_data.WriteLine(i + ".rtf");
+                    }
+                }
+                catch (System.OverflowException)
+                {
+                    using (StreamWriter overflow = new StreamWriter(@"Users\mac\Projects\Lab_4_1\Lab_4_1\txt\overflow.txt", true, System.Text.Encoding.Default))
+                    {
+                        overflow.WriteLine(i + ".txt");
+                    }
+                }
+                catch (System.IO.IOException)
+                {
+                    Console.WriteLine("Файл відсутній.");
+                    break;
+                }
+            }
+            Console.WriteLine("Кількість нормальних файлів - {0}. Середнє арифметичне добутку двох чисел з цих файлів - {1}", n, sum / n);
+
+        }
+    }
+}
---
+++ Half1/Лр4/Lab_4_2/Lab_4_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Drawing.Imaging;
+
+
+namespace Lab_4_2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] array1 = Directory.GetFiles(@"Users\mac\Projects\Lab_4_2\Lab_4_2\photos");
+            string[] names = Directory.GetFiles(@"Users\mac\Projects\Lab_4_2\Lab_4_2\photos").Select(Path.GetFileName).ToArray();
+            Regex regexExtForImage = new Regex("^((.bmp)|(.gif)|(.tiff?)|(.jpe?g)|(.png))$", RegexOptions.IgnoreCase);
+            for (int i = 0; i < array1.Length; i++)
+            {
+                try
+                {
+                    if (regexExtForImage.IsMatch(Path.GetExtension(array1[i])))
+                    {
+                        Bitmap bitmap1 = (Bitmap)Bitmap.FromFile(array1[i]);
+                        bitmap1.RotateFlip(RotateFlipType.Rotate180FlipY);
+                        object p = bitmap1.Save(@"Users\mac\Projects\Lab_4_2\Lab_4_2\photos" + names[i].Split('.')[1] + "-mirrored.png", ImageFormat.Png);
+                        Console.WriteLine("Утворено нове фото та збережено");
+                    }
+                }
+                catch (System.OutOfMemoryException)
+                {
+                    Console.WriteLine("Файл не містить картинки, хоча розширення відповідає");
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
