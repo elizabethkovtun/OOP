e48860807991ccfde06e3fb31ed3337e5cd5654c
Elizabeth Kovtun <58321670+elizabethkovtun@users.noreply.github.com>
2021-03-23 19:31:17 +0200

    Add files via upload

---
+++ Half2/Лр3/Part1/Lab_3.1_ex_6/Lab_3.1_ex_6/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_3._1_ex_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            DialogResult aResult;
+            Form2 aForm = new Form2();
+            aResult = aForm.ShowDialog();
+            if(aResult==System.Windows.Forms.DialogResult.OK)
+            {
+                MessageBox.Show("Yourr name is" + aForm.textBox1.Text + " " + aForm.textBox2.Text);
+            }
+            linkLabel1.LinkVisited = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+            System.Diagnostics.Process.Start("http://www.google.com/");
+
+            linkLabel2.LinkVisited = true;
+        }
+    }
+}
