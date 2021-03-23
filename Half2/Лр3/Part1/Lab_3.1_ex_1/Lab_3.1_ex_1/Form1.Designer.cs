adafb7d0b696790334800630fcee8e369191ff56
Elizabeth Kovtun <58321670+elizabethkovtun@users.noreply.github.com>
2021-03-23 12:48:05 +0200

    Add files via upload

---
+++ Half2/Лр3/Part1/Lab_3.1_ex_1/Lab_3.1_ex_1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Lab_3._1_ex_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnyes = new System.Windows.Forms.Button();
+            this.btnno = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnyes
+            // 
+            this.btnyes.Location = new System.Drawing.Point(60, 101);
+            this.btnyes.Name = "btnyes";
+            this.btnyes.Size = new System.Drawing.Size(86, 35);
+            this.btnyes.TabIndex = 0;
+            this.btnyes.Text = "Так";
+            this.btnyes.UseVisualStyleBackColor = true;
+            this.btnyes.Click += new System.EventHandler(this.btnyes_Click);
+            this.btnyes.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnyes_MouseMove);
+            // 
+            // btnno
+            // 
+            this.btnno.Location = new System.Drawing.Point(169, 101);
+            this.btnno.Name = "btnno";
+            this.btnno.Size = new System.Drawing.Size(86, 35);
+            this.btnno.TabIndex = 1;
+            this.btnno.Text = "Ні";
+            this.btnno.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ви задоволені своєю заробітною платою?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 157);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnno);
+            this.Controls.Add(this.btnyes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Нагальне питання";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnyes;
+        private System.Windows.Forms.Button btnno;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Half2/Лр3/Part1/Lab_3.1_ex_2/Lab_3.1_ex_2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_3._1_ex_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if(peopleList.Text.Length!=0)
+            {
+                membersList.Items.Add(peopleList.Text);
+            }
+            else
+            {
+                MessageBox.Show("Виберіть елемент зі списку або введіть новий");
+            }
+
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            while(membersList.CheckedIndices.Count>0)
+            {
+                membersList.Items.RemoveAt(membersList.CheckedIndices[0]);
+            }
+        }
+
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            membersList.Sorted = true;
+        }
+    }
+}
