cfd5a09655f802cdcda5ea5ac525b48efb5f4b08
Elizabeth Kovtun <58321670+elizabethkovtun@users.noreply.github.com>
2021-03-23 20:04:13 +0200

    Add files via upload

---
+++ Half2/Лр3/Part1/Lab_3.1_ex_7/Lab_3.1_ex_7/Form2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab_3._1_ex_6
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
