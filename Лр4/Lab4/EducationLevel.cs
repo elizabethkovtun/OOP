d89cd569d256f20b8fec6ce53be51dd283126927
Elizabeth Kovtun <58321670+elizabethkovtun@users.noreply.github.com>
2021-06-04 22:12:56 +0300

    Add files via upload

---
+++ Лр4/Lab4/EducationLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+   public enum EducationLevel
+    {
+        Бакалавр,
+        Специалист,
+        Магистр
+    }
+}
